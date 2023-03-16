f290e0be15f4518102cf02989eb97db3cc3efc89
RonaldoDairo <programaciondairo@gmail.com>
2023-03-16 00:16:57 +0000

    agg avance

--- src/js/component/CardPeople.jsx
+++ src/js/component/CardPeople.jsx
@@ -3,9 +3,9 @@ import { Card } from "./card.jsx";
 export const CardPeople = () =>{
 
         return (
-                <div className="container">
-                    <div id="carrousel" className="card-group"> 
-                        <Card/> <Card/><Card/> <Card/><Card/> <Card/><Card/> <Card/><Card/><Card/><Card/><Card/>
+                <div className="container col-12 sm-6">
+                    <div id="carrousel" className="card-group "> 
+                        <Card/> <Card/><Card/> <Card/><Card/> <Card/><Card/> <Card/><Card/><Card/><Card/><Card/> <Card/> <Card/><Card/> <Card/><Card/> <Card/><Card/> <Card/><Card/><Card/><Card/><Card/>
                        
                     </div>
                 </div>
--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,23 +1,17 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 
 export const Card = () => (
-	<div className="card" >
-				<img src="..." className="card-img" alt="..." />
-				<div className="card-body">
-				<h5 className="card-title">Card title</h5>
-				<p className="card-text text-center">Some quick </p>
-				<a href="#" className="btn btn-primary">Go somewhere</a>
+	
+	<div className="cardpeople col-md-6 col-sm-6  " >
+		<div class="card ml-5 text-center bg-transparent border-0 d-flex col-md-6" >
+				<img src="https://images.unsplash.com/photo-1454923634634-bd1614719a7b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cGVvcGxlJTIwYXQlMjB3b3JrfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" className="card-img" alt="..." />
+				<div className="card-body cards">
+				<h5 className="card-title text-uppercase">Card title</h5>
+				<p className="card-text">Some quick </p>
+				<a href="#" id="boton" className="btn btn-primary">Go somewhere</a>
 				</div>
+		</div>
 	</div>
 );
-{/* <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div> */}
